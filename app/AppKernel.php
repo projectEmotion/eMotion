d9c18d6cfa3b4e19dbf2ef9671877c2704d29e20
Jed TASSA <jed.tassa@idtgv.com>
2017-09-26 12:24:09 +0200

    Intégration de Sonata + création d'AdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,12 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
               new FOS\UserBundle\FOSUserBundle(),
+              // These are the other bundles the SonataAdminBundle relies on
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new AdminBundle\AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
