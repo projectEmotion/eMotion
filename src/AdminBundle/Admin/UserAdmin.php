a48fed01cb7238204ebbedb3f7047c1ff53e7328
Jed Tassa <jed.tassa@idtgv.com>
2017-09-28 10:06:51 +0200

    Modif UserAdmin

--- src/AdminBundle/Admin/UserAdmin.php
+++ src/AdminBundle/Admin/UserAdmin.php
@@ -9,10 +9,36 @@ use Sonata\AdminBundle\Form\FormMapper;
 
 class UserAdmin extends AbstractAdmin
 {
+    public function toString($object)
+    {
+        return $object instanceof User
+            ? $object->getTitle()
+            : 'User'; // shown in the breadcrumb on the create view
+    }
     protected function configureFormFields(FormMapper $formMapper)
     {
+        $formMapper->add('username','text');
+        $formMapper->add('email','text');
+        $formMapper->add('password','text');
         $formMapper->add('adress','text');
+        $formMapper->add('phone','text');
+        $formMapper->add('codePostal','text');
+        $formMapper->add('Ville','text');
         $formMapper->add('numberLicence','text');
         $formMapper->add('dateOfBirth','date');
+
+    }
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('username');
+        $listMapper->addIdentifier('email');
+        $listMapper->addIdentifier('password');
+        $listMapper->addIdentifier('adress');
+        $listMapper->addIdentifier('phone');
+        $listMapper->addIdentifier('codePostal');
+        $listMapper->addIdentifier('Ville');
+        $listMapper->addIdentifier('numberLicence');
+        $listMapper->addIdentifier('dateOfBirth');
+        $listMapper->addIdentifier('id');
     }
 }
