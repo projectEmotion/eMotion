93f4708383a48ba82565f82e7604669dbb7adfb0
Moad <Moad@gmail.com>
2017-10-30 09:38:19 +0100

    no message

--- src/FrontBundle/Controller/PaymentController.php
+++ src/FrontBundle/Controller/PaymentController.php
@@ -12,4 +12,30 @@ class PaymentController extends Controller
     //     dump($session);
     //     return new Response('Ok');
     // }
+
+       public function indexAction()
+       {
+        $request = $this->container->get('request');
+        $message = '';
+        if($request->get('test'))
+        {
+            Stripe::setApiKey('pk_test_1uUx58I2mZLsuEbM5VwP1GU2');
+
+            $token = $request->get('stripeToken');
+
+            $customer = \Stripe_Customer::create(array(
+                  'email' => 'customer@example.com',
+                  'card'  => $token
+            ));
+
+            $charge = Stripe_Charge::create(array(
+                  'customer' => $customer->id,
+                  'amount'   => 5000,
+                  'currency' => 'usd'
+            ));
+
+            $message = '<h1>Successfully charged $50.00!</h1>';
+        }
+
+       }
 }
