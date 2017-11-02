61e9c9b6d8cb9dd12f30013731e343f7d3ef32b8
Jed Tassa <jed.tassa@idtgv.com>
2017-11-02 18:54:14 +0100

    Paiement accepté mais problème de route

--- src/FrontBundle/Controller/OrderController.php
+++ src/FrontBundle/Controller/OrderController.php
@@ -17,43 +17,40 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class OrderController extends Controller
 {
-    /**
-     * @Route("/prepare", name="order_prepare")
-     */
+   
     public function prepareAction()
     {
         return $this->render('FrontBundle:Default:prepare.html.twig');
     }
 
-    /**
-     * @Route(
-     *     "/checkout",
-     *     name="order_checkout",
-     *     methods="POST"
-     * )
-     */
+    
     public function checkoutAction(Request $request)
     {
 
-        \Stripe\Stripe::setApiKey("sk_test_R5Nro9XcIeikCo92YEP4beyW");
-
+        \Stripe\Stripe::setApiKey("sk_test_Mtp40DUIRNqfrkWgxvmZWLyG");
+        
         // Get the credit card details submitted by the form
-        $token = $_POST['stripeToken'];
-
+        $token = $request->get('stripeToken');
         // Create a charge: this will charge the user's card
         try {
+
+            
+        // Create a Customer:
+        $customer = \Stripe\Customer::create(array(
+        "email" => "paying.user@example.com",
+        "source" => $token,
+        ));
             $charge = \Stripe\Charge::create(array(
-                "amount" => 1000, // Amount in cents
+                "amount" => 10000, // Amount in cents
                 "currency" => "eur",
-                "source" => $token,
-                "description" => "Paiement Stripe - OpenClassrooms Exemple"
+                "customer" => $customer->id
             ));
             $this->addFlash("success","Bravo ça marche !");
-            return $this->redirectToRoute("order_prepare");
+            return $this->redirectToRoute("order_checkout");
         } catch(\Stripe\Error\Card $e) {
-
+            
             $this->addFlash("error","Snif ça marche pas :(");
-            return $this->redirectToRoute("order_prepare");
+            return $this->redirectToRoute("prepare_page");
             // The card has been declined
         }
     }
