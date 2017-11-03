bf8a3c0b4851796ab62f1165016fae2fcc974716
Ronald <ronaldrlrj.123@gmail.com>
2017-11-03 14:09:44 +0100

    dernier modi

--- src/FrontBundle/Controller/OrderController.php
+++ src/FrontBundle/Controller/OrderController.php
@@ -24,10 +24,26 @@ class OrderController extends Controller
         return $this->render('FrontBundle:Default:prepare.html.twig');
     }
 
-    
+    public function Session()
+    {
+        $frontSession = $this->get('front.session');
+        $frontSession->initiate($this->get('session'));
+        
+        return $frontSession;
+    }
+    public function checkoutSuccessAction()
+    {
+        return $this->render('FrontBundle:Default:checkout.html.twig');
+    }
     public function checkoutAction(Request $request)
     {
-
+        $cart = $this->Session()->getCart();
+        $totalPrice = 0;
+        if(!empty($cart)){
+            foreach ($cart as $c){
+                $totalPrice+= $c->getPrice();
+            }
+        }
         \Stripe\Stripe::setApiKey("sk_test_Mtp40DUIRNqfrkWgxvmZWLyG");
         
         // Get the credit card details submitted by the form
@@ -35,19 +51,20 @@ class OrderController extends Controller
         // Create a charge: this will charge the user's card
         try {
 
-            
+        $amount = $totalPrice*100;    
         // Create a Customer:
         $customer = \Stripe\Customer::create(array(
         "email" => "paying.user@example.com",
         "source" => $token,
         ));
             $charge = \Stripe\Charge::create(array(
-                "amount" => 10000, // Amount in cents
+                "amount" => $amount, // Amount in cents
                 "currency" => "eur",
                 "customer" => $customer->id
             ));
             $this->addFlash("success","Bravo ça marche !");
-            return $this->redirectToRoute("order_checkout");
+            return $this->redirect($this->generateUrl('order_checkout_success'));
+            //return $this->redirectToRoute("order_checkout");
         } catch(\Stripe\Error\Card $e) {
             
             $this->addFlash("error","Snif ça marche pas :(");
